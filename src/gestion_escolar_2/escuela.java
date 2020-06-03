84573980d2b707995653e196ebfb6651ddd5523d
YairReyes <66081359+YairReyes@users.noreply.github.com>
2020-06-03 13:23:26 -0500

    Add:escuela

--- src/gestion_escolar_2/escuela.java
+++ src/gestion_escolar_2/escuela.java
@@ -10,5 +10,45 @@ package gestion_escolar_2;
  * @author User
  */
 public class escuela {
-    
+     private String nombre,direccion;
+    private int clave,telefono;
+    public void escuela(String nombre,String direccion,int clave,int telefono){
+        this.nombre = this.nombre;
+        this.direccion = this.direccion;
+        this.clave = this.clave;
+        this.telefono = this.telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public int getClave() {
+        return clave;
+    }
+
+    public void setClave(int clave) {
+        this.clave = clave;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+   
 }
