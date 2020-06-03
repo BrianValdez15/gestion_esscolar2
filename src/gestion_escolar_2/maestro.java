9d3fca23de9b6919dda7b877b0515f7d0d0e58b9
YairReyes <66081359+YairReyes@users.noreply.github.com>
2020-06-03 13:22:00 -0500

    Add:maestro

--- src/gestion_escolar_2/maestro.java
+++ src/gestion_escolar_2/maestro.java
@@ -10,5 +10,52 @@ package gestion_escolar_2;
  * @author User
  */
 public class maestro extends escuela{
+    private  String Correo;
+  private  String Nombre;
+ private   String Contraseña;
+  private  int Tel;
+  private int IdProfe;
+
+    public maestro(String Correo,String Nombre, String Contraseña, int Tel ) {
+        this.Correo = Correo;
+        this.Nombre = Nombre;
+        
+        this.Contraseña = Contraseña;
+        this.Tel = Tel;
+    }
+public maestro (int IdProfe){
+    this.IdProfe= IdProfe;
+}
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String Correo) {
+        this.Correo = Correo;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public String getContraseña() {
+        return Contraseña;
+    }
+
+    public void setContraseña(String Contraseña) {
+        this.Contraseña = Contraseña;
+    }
+
+    public int getTel() {
+        return Tel;
+    }
+
+    public void setTel(int Tel) {
+        this.Tel = Tel;
+}
     
 }
