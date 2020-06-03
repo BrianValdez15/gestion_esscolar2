a6a3835f5f69b8eef1d865f034b2a06ab5221a8e
YairReyes <66081359+YairReyes@users.noreply.github.com>
2020-06-03 13:22:44 -0500

    Add:horario

--- src/gestion_escolar_2/horario.java
+++ src/gestion_escolar_2/horario.java
@@ -10,5 +10,31 @@ package gestion_escolar_2;
  * @author User
  */
 public class horario extends clase{
-    
+      String Dia;
+   double Inicio,Termino;
+
+    public Horario(int Salon, int grado) { //Curso
+        super(Salon, grado);
+    }
+   
+    public void Dia() { 
+    String D;
+          System.out.println("Lunes");
+
+    }
+    public void Inicio() { 
+    double Inicia;
+      double I = 8.50;
+         System.out.println(I);
+
+    }
+    public void Termino() { 
+      double Terino =9.45 ; 
+         System.out.println(Termino);
+   }
+  public void MostrarHorario(){
+  System.out.println(Dia);
+    System.out.println(Inicio);
+    System.out.println(Termino);
+  }
 }
